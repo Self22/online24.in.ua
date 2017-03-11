25dfd90e268984ad8486a001fc97295b4350865d
Eugen <evgeniy.sayenko@gmail.com>
2017-03-11 20:43:35 +0200

    ЛИГА почти закончена, начал ваять валидацию форм

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -56,6 +56,10 @@ $('.bp-hs').bpHS(autoPlay = true, showButtons = true, showControls = true, showB
 //parallaz
 
 $('.header').parallax({imageSrc: 'img/whiteblue_back606.png'});
+$('.liga__header-content').parallax({imageSrc: 'img/liga_header.jpg'});
+$('.liga__fotoblock').parallax({imageSrc: 'img/liga__block1.jpg'});
+
+
 // $('.footer').parallax({imageSrc: 'img/whiteblue_back602.png'});
 
 //sticky header
@@ -122,4 +126,67 @@ $(function () {
 
     });
 
+});
+
+$(document).ready(function () {
+
+    $("#loginform").validate({
+
+        rules: {
+
+            login: {
+                required: true,
+                minlength: 4,
+                maxlength: 16,
+            },
+
+            pswd: {
+                required: true,
+                minlength: 6,
+                maxlength: 16,
+            },
+        },
+
+        messages: {
+
+            login: {
+                required: "Это поле обязательно для заполнения",
+                minlength: "Логин должен быть минимум 4 символа",
+                maxlength: "Максимальное число символо - 16",
+            },
+
+            pswd: {
+                required: "Это поле обязательно для заполнения",
+                minlength: "Пароль должен быть минимум 6 символа",
+                maxlength: "Пароль должен быть максимум 16 символов",
+            },
+
+        }
+
+    });
+
+});
+
+$(function () {
+
+    $('#footerform').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 3
+            }
+        },
+        messages: {
+            name: {
+                required: "Поле 'Имя' обязательно к заполнению",
+
+                minlength: "Введите не менее 3-х символов в поле 'Имя'"
+            },
+            email: {
+                required: "Поле 'Email' обязательно к заполнению",
+
+                email: "Неверный формат адреса email"
+            }
+        }
+    });
 });
