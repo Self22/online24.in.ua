15eb7a8ceee59f7d0212dfe444dbf0e8791dffb8
Eugen <evgeniy.sayenko@gmail.com>
2017-04-23 11:37:35 +0300

    Оттачиваем ЛИГОвские лендинги, заканчиваю моб. адаптив

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -211,54 +211,54 @@ $(function () {
 
 /////////////// mask
 $(document).ready(function () {
-    $('.footer__form-tel').mask('+38(000) 000-00-00');
+    $('.footer__form-tel').mask('+38 (000) 000-00-00');
 });
 
 ////// slow scroll
 
-$(document).ready(function () {
-
-    var container = $('.liga__header');
-
-    $(".lmain__menu").on("click", ".menu__inner", function (event) {
-        //отменяем стандартную обработку нажатия по ссылке
-
-        event.preventDefault();
-        //забираем идентификатор блока с атрибута href
-        var id = $(this).attr('href'),
-        //узнаем высоту от начала страницы до блока на который ссылается якорь
-            top = $(id).offset().top - 100;
-        //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
-
-    $(".navbar-toggleable-sm").on("click", ".menu__inner", function (event) {
-        //отменяем стандартную обработку нажатия по ссылке
-
-        event.preventDefault();
-        //забираем идентификатор блока с атрибута href
-        var id = $(this).attr('href'),
-        //узнаем высоту от начала страницы до блока на который ссылается якорь
-            top = $(id).offset().top;
-        //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({scrollTop: top}, 1500);
-        setTimeout(container.removeClass("in"), 500);
-    });
-
-    $(".navbar-toggleable-sm").on("tap", ".menu__inner", function (event) {
-        //отменяем стандартную обработку нажатия по ссылке
-
-        event.preventDefault();
-        //забираем идентификатор блока с атрибута href
-        var id = $(this).attr('href'),
-        //узнаем высоту от начала страницы до блока на который ссылается якорь
-            top = $(id).offset().top;
-        //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
-    
-});
-
+// $(document).ready(function () {
+//
+//     var container = $('.liga__header');
+//
+//     $(".lmain__menu").on("click", ".menu__inner", function (event) {
+//         //отменяем стандартную обработку нажатия по ссылке
+//
+//         event.preventDefault();
+//         //забираем идентификатор блока с атрибута href
+//         var id = $(this).attr('href'),
+//         //узнаем высоту от начала страницы до блока на который ссылается якорь
+//             top = $(id).offset().top - 100;
+//         //анимируем переход на расстояние - top за 1500 мс
+//         $('body,html').animate({scrollTop: top}, 1500);
+//     });
+//
+//     $(".navbar-toggleable-sm").on("click", ".menu__inner", function (event) {
+//         //отменяем стандартную обработку нажатия по ссылке
+//
+//         event.preventDefault();
+//         //забираем идентификатор блока с атрибута href
+//         var id = $(this).attr('href'),
+//         //узнаем высоту от начала страницы до блока на который ссылается якорь
+//             top = $(id).offset().top;
+//         //анимируем переход на расстояние - top за 1500 мс
+//         $('body,html').animate({scrollTop: top}, 1500);
+//         setTimeout(container.removeClass("in"), 500);
+//     });
+//
+//     $(".navbar-toggleable-sm").on("tap", ".menu__inner", function (event) {
+//         //отменяем стандартную обработку нажатия по ссылке
+//
+//         event.preventDefault();
+//         //забираем идентификатор блока с атрибута href
+//         var id = $(this).attr('href'),
+//         //узнаем высоту от начала страницы до блока на который ссылается якорь
+//             top = $(id).offset().top;
+//         //анимируем переход на расстояние - top за 1500 мс
+//         $('body,html').animate({scrollTop: top}, 1500);
+//     });
+//
+// });
+//
 
 $(document).ready(function () {
 
@@ -354,4 +354,6 @@ $(document).ready(function () {
 
 ///////////////////////////
 
-$('#nav-main').singlePageNav();
+$('#nav-main').singlePageNav(
+    {offset: 100, speed: 1000, threshold: 150}
+);
