fbdf872ce63d63c71143ee75f97b24d96f0efe97
Eugen <evgeniy.sayenko@gmail.com>
2017-05-05 23:43:33 +0300

    Промежуточны коммит Карпаты

--- prod/js/jquery.liLanding.js
+++ prod/js/jquery.liLanding.js
@@ -1,2 +1,112 @@
-!function(t){var o={init:function(o){var n={show:function(t,o){},hide:function(t,o){},topMargin:0,speedFactor:1};return o&&t.extend(n,o),this.each(function(){var o=t(this),i=o.offset().top,e=t(window).height()/2,r=function(){};t("a[href^=#]",o).on("click",function(){var o=t(this);if(!o.is(".cur")){var i=o.attr("href"),e=t(i),r=e.offset().top,a=t(window).scrollTop(),s=r-a;s<0&&(s*=-1),e.length&&t("html, body").stop(!0).animate({scrollTop:r-parseFloat(n.topMargin)},s*n.speedFactor,function(){t(window).trigger("scroll")})}return!1});var a=/Firefox/i.test(navigator.userAgent)?"DOMMouseScroll":"mousewheel";document.attachEvent?document.attachEvent("on"+a,function(o){t("html, body").stop(!0)}):document.addEventListener&&document.addEventListener(a,function(o){t("html, body").stop(!0)},!1),t(window).on("scroll",function(a){clearTimeout(r);var s=t(window).scrollTop();s>i?o.addClass("landingFix"):o.removeClass("landingFix"),r=setTimeout(function(){t(".landingItem").each(function(){var i=t(this),r=i.height(),a=i.offset().top-e,c=i.attr("id"),d=t('a[href="#'+c+'"]',o);s>a&&s<a+r?d.is(".cur")||(d.addClass("cur"),void 0!==n.show&&n.show(d,i)):d.is(".cur")&&(d.removeClass("cur"),void 0!==n.hide&&n.hide(d,i))})},100)}),t(window).trigger("scroll")})}};t.fn.liLanding=function(n){return o[n]?o[n].apply(this,Array.prototype.slice.call(arguments,1)):"object"!=typeof n&&n?void t.error("Метод "+n+" в jQuery.liLanding не существует"):o.init.apply(this,arguments)}}(jQuery);
+/*
+ * jQuery liLanding v 2.1
+ *
+ * Copyright 2013, Linnik Yura | LI MASS CODE | http://masscode.ru
+ * Free to use
+ *
+ * Last Update: 19.06.2016
+ */
+(function ($) {
+    var methods = {
+        init: function (options) {
+            var p = {
+                show: function (linkEl, landingItem) {
+                },
+                hide: function (linkEl, landingItem) {
+                },
+                topMargin: 0,
+                speedFactor: 1
+            };
+            if (options) {
+                $.extend(p, options);
+            }
+            return this.each(function () {
+                var el = $(this);
+                var elPos = el.offset().top;
+                var wHalf = $(window).height() / 2
+                var scrollId = function () {
+                };
+
+                //assign events only links with anchors
+                $('a[href^=#]', el).on('click', function () {
+                    var linkItem = $(this);
+                    if (!linkItem.is('.cur')) {
+                        var linkHref = linkItem.attr('href');
+                        var linkTarget = $(linkHref);
+                        var linkTargetPos = linkTarget.offset().top;
+                        var windowPos = $(window).scrollTop();
+                        var animDuration = linkTargetPos - windowPos
+                        if (animDuration < 0) {
+                            animDuration = animDuration * -1
+                        }
+                        //scroll the page to the desired block
+                        if (linkTarget.length) {
+                            $('html, body').stop(true).animate({scrollTop: (linkTargetPos - parseFloat(p.topMargin))}, animDuration * p.speedFactor, function () {
+                                $(window).trigger('scroll');
+                            });
+                        }
+                    }
+                    return false;
+                })
+                //stop the animation by scrolling
+                var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
+                if (document.attachEvent) //if IE (and Opera depending on user setting)
+                    document.attachEvent("on" + mousewheelevt, function (e) {
+                        $('html, body').stop(true);
+                    });
+                else if (document.addEventListener) //WC3 browsers
+                    document.addEventListener(mousewheelevt, function (e) {
+                        //e.detail //direction
+                        $('html, body').stop(true);
+                    }, false)
+                //highlight the desired link in the menu by scrolling
+                $(window).on('scroll', function (e) {
+                    clearTimeout(scrollId);
+                    var windowPos = $(window).scrollTop();
+                    if (windowPos > elPos) {
+                        el.addClass('landingFix');
+                    } else {
+                        el.removeClass('landingFix');
+                    }
+                    scrollId = setTimeout(function () {
+                        $('.landingItem').each(function () {
+                            var landingItem = $(this);
+                            var landingItemHeight = landingItem.height();
+                            var landingItemTop = landingItem.offset().top - wHalf;
+                            var linkHref = landingItem.attr('id');
+                            var linkEl = $('a[href="#' + linkHref + '"]', el);
+                            var status;
+
+                            if (windowPos > landingItemTop && windowPos < (landingItemTop + landingItemHeight)) {
+                                if (!linkEl.is('.cur')) {
+                                    linkEl.addClass('cur');
+                                    if (p.show !== undefined) {
+                                        p.show(linkEl, landingItem);
+                                    }
+                                }
+                            } else {
+                                if (linkEl.is('.cur')) {
+                                    linkEl.removeClass('cur');
+                                    if (p.hide !== undefined) {
+                                        p.hide(linkEl, landingItem);
+                                    }
+                                }
+                            }
+                        });
+                    }, 100);
+                })
+                $(window).trigger('scroll');
+            });
+        }
+    };
+    $.fn.liLanding = function (method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Метод ' + method + ' в jQuery.liLanding не существует');
+        }
+    };
+})(jQuery);
 //# sourceMappingURL=jquery.liLanding.js.map
